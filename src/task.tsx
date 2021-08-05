4f0ca61db6592d7ba7dfa40e40a7220e14dc8c37
Som3on31 <saharit.f4@gmail.com>
2021-08-05 18:15:58 +0700

    Added a function to toggle buttons' visibility

--- src/task.tsx
+++ src/task.tsx
@@ -1,27 +1,24 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 type task2Type = {
     name: string;
 }
 
-// const [visible, setVisible] = useState(false)
-
-// function show(){
-//     setVisible(true)
-// }
-
-// function hide(){
-//     setVisible(false)
-// }
-
-
 function Task(task: task2Type) {
+    const [visible, setVisible] = useState(false)
+
+    const [done, setDone] = useState(false)
+
+    const show = () => setVisible(true)
+
+    const hide = () => setVisible(false)
+
     return (
-        <div className="flex justify-between h-8 items-center py-6 border-b">
+        <div className="flex justify-between h-8 items-center py-6 border-b" onMouseEnter={show} onMouseLeave={hide}>
             <span className="text-2xl"> {task.name} </span>
             <div className="flex space-x-1 items-center">
-                <button className="bg-green-400 w-24 text-2xl" >Done</button>
-                <button className="bg-red-400 w-24 text-2xl" >Delete</button>
+                <button className="bg-green-400 w-24 text-2xl">{visible && done!=true ? 'Done' : ''}</button>
+                <button className="bg-red-400 w-24 text-2xl">{visible && done!=true ? 'Delete' : ''}</button>
             </div>
         </div>
     )
